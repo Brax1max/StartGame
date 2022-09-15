5bc529a3b152d508a2ef5f4277c6a2c92929a7fd
braxdaes073 <braxdaes073@SDS-L-BB1HC63>
2022-09-15 10:57:42 -0400

    Add project files.

---
+++ StartGame/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace StartGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startLabel
+            // 
+            this.startLabel.Location = new System.Drawing.Point(89, 9);
+            this.startLabel.Name = "startLabel";
+            this.startLabel.Size = new System.Drawing.Size(692, 144);
+            this.startLabel.TabIndex = 0;
+            this.startLabel.Text = "Start game";
+            this.startLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(309, 195);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(248, 89);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 356);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.startLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label startLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ StartGame/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace StartGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            startLabel.Text = "3"; 
+            
+            Refresh();
+            Thread.Sleep(1000);
+
+            startLabel.Text = "2";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            startLabel.Text = "1";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+            startLabel.Text = "go";
+
+            Refresh();
+            Thread.Sleep(1000);
+
+
+        }
+    }
+}
